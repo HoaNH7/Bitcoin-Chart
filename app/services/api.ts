0a82f9f04c1754aad50655681b23c91895e23198
HoaNH7 <137791452+HoaNH7@users.noreply.github.com>
2025-02-28 09:57:13 +0700

    Update api.ts

--- app/services/api.ts
+++ app/services/api.ts
@@ -13,14 +13,17 @@ export const fetchBitcoinData = async (interval: string = "1m", limit: number = 
       },
     });
 
-    return response.data.map((data: any) => ({
-      time: (data[0] / 1000) as UTCTimestamp, 
+    type KlineData = [number, string, string, string, string, string];
+
+    return response.data.map((data: KlineData) => ({
+      time: (data[0] / 1000) as UTCTimestamp,
       open: parseFloat(data[1]),
       high: parseFloat(data[2]),
       low: parseFloat(data[3]),
       close: parseFloat(data[4]),
       volume: parseFloat(data[5]),
     }));
+    
   } catch (error) {
     console.error("Lỗi khi lấy dữ liệu Bitcoin:", error);
     return [];
